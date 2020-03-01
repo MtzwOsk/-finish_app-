ed5825ec3580492b17154340dbeec2be361b2861
tmazgaj <tmazgaj@ic.eu>
2020-03-01 22:40:56 +0100

    Part1 anecdotes

--- src/index.js
+++ src/index.js
@@ -1,49 +1,47 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
 
-const Button = ({handlerClick, text}) => (
-  <button onClick={handlerClick}>{text}</button>
-)
+const Button = ({length, onClick}) => {
+  let selected = Math.floor((Math.random() * length))
+  return (
+    <button onClick={() => onClick(selected)}>next anecdote</button>
+  )
+}
 
-const Display = ({value, text}) => (
-  <p>{text}: {value}</p>
-)
 
-const Statistics = ({good, bad, neutral}) => {
-  const sum = good + bad + neutral
-  let avarge = 0
-  let positives = good / sum
-  if ({sum} !== 0) {
-    avarge = (good * 1 + bad * -1 + neutral * 0) / sum
-  }
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+  const length = anecdotes.length
+  const [points, upVote] = useState(Array.from({length:length},(v,k)=>k).reduce((o, key) => ({ ...o, [key]: 0}), {}))
+
+
   return (
     <div>
-      <p>All: {sum}</p>
-      <p>avarge: {avarge}</p>
-      <p>Positives: {positives} %</p>
+      <p>{props.anecdotes[selected]}</p>
+      <Button length={length} onClick={setSelected}/>
+      <button onClick={() => upVote({...points, selected: points[selected] += 1})}>vote</button>
+      <p>has {points[selected]}</p>
+      <div>
+        <p>Anecdote with most votes</p>
+        <p>{props.anecdotes[Object.keys(points).filter((x) => (points[x] ===  Math.max(...Object.values(points))))[0]]}</p>
+      </div>
     </div>
   )
 }
 
-const App = () => {
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  return (
-    <div>
-      <Button handlerClick={() => setGood(good + 1)} text='good'/>
-      <Button handlerClick={() => setNeutral(neutral + 1)} text='neutral'/>
-      <Button handlerClick={() => setBad(bad + 1)} text='bad'/>
-      <Display value={good} text='good'/>
-      <Display value={neutral} text='neutral'/>
-      <Display value={bad} text='bad'/>
-      <Statistics good={good} bad={bad} neutral={neutral}/>
-    </div>
-  )
-}
 
-ReactDOM.render(<App />, 
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
   document.getElementById('root')
 )
